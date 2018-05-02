72e267561a7e807a45f5ccae45a7d83d21db701e
BakaMondai <reppersonwo@gmail.com>
2018-05-01 22:33:23 -0500

    fixed ticket parser

--- lottery/ticket_parser.py
+++ lottery/ticket_parser.py
@@ -1,21 +1,30 @@
 import requests
 import urllib.request
 import export
+import time
 import config
 from bs4.element import Comment
 from bs4 import BeautifulSoup
 
 def lottery_access(login_info, posting_info):
-  for i in range(0, config.counter, 1):
-    with requests.Session() as session:
-      #login to the website
-      session.post(config.login_url, data=login_info)
+  while True:
+    try:
+      counter = int(input("Enter something: "))
+    except ValueError:
+      print("Not an integer!")
+      continue
+    else:
+      print("Yes an integer!")
+      break 
 
-      #request the information from the url
-      hex_rpg_response = session.get(config.lottery_url)
+
+  with requests.Session() as session:
+    #login to the website
+    session.post(config.login_url, data=login_info)
     
+    for i in range(0, counter, 1):
       #post our  information to the lottery ticket page
-      hex_rpg_lottery_post = session.post(config.lottery_url, data=posting_info)
-      print(f"Purchased ticket: {i + 1}")
+      session.post(config.lottery_url, data=posting_info)
+      print(f"Purchased ticket: {i + 1}")    
 
 lottery_access(config.login_info, config.purchase_post)
